9f7bc5b9a806050ff70c18993ff8ba914ee4f00a
KoreyBulloch <a-koreyb@hotmail.com>
2015-12-11 18:32:01 -0800

    Cleaned up and updated comments

--- herokuapp/test_herokuapp_iframe_tinymce_extended.py
+++ herokuapp/test_herokuapp_iframe_tinymce_extended.py
@@ -13,25 +13,24 @@ time_date = time.strftime("%Y-%m-%d")
 time_stamp = time_date + "_" + time_hours
 
 
-# Clear text, select Bold from the Format menu, type "Bold text is bold",
-# take a screenshot and save it with a time stamp in the name, and verify
-# that we write "Bold text is bold".
+# Verify typed text after selecting Bold from the menu and typing in bold.
 def test_tinymce_menu_bold_text(selenium, variables, util):
-    # This will be used as part of the name for all screenshots taken.
+    # This will be used as part of the name for all screenshots taken for
+    # this test.
     test_name = "TinyMCE_menu_bold_text_"
     # Click on "WYSIWYG Editor".
     selenium.find_element_by_xpath(variables["wysiwyg_editor_url"]).click()
     # Switching focus:
     # Select TinyMCE iframe.
     selenium.switch_to.frame(selenium.find_element_by_xpath(variables["tinymce_iframe"]))
-    # Select the TinyMCE text field and clear existing text.
+    # Clear existing text in the TinyMCE text field.
     selenium.find_element_by_xpath(variables["tinymce_text_field"]).clear()
     # Switching focus:
-    # Go back to the webpage/default content
+    # Go back to the webpage/default content.
     selenium.switch_to.default_content()
-    # Select the "Format" menu and click on it.
+    # Click the "Format" menu.
     selenium.find_element_by_xpath(variables["format_menu"]).click()
-    # Select "Bold" from the Format menu and click on it.
+    # Click on "Bold" in the Format menu.
     # Note: TinyMCE is a complete pain.  Button IDs seem to be generated
     # on load, names have spaces, and all sorts of other headaches.  For
     # this we're using XPath and text contains, which could be refined
@@ -40,12 +39,13 @@ def test_tinymce_menu_bold_text(selenium, variables, util):
     # Switching focus:
     # Select TinyMCE iframe.
     selenium.switch_to.frame(selenium.find_element_by_xpath(variables["tinymce_iframe"]))
-    # Select the TinyMCE text field and type "I <3 iframes".
+    # Select the TinyMCE text field and type "Bold text is bold".
     selenium.find_element_by_xpath(variables["tinymce_text_field"]).send_keys("Bold text is bold")
-    # Save a screenshot with our custom file name and time_stamp.
+    # Save a screenshot with our custom file name and time_stamp in the current working directory.
     selenium.save_screenshot(test_name + time_stamp + ".png")
-    # Save a screenshot with our custom file name and time_stamp to a specific folder,
-    # (screenshots in the test directory).  This is PC only for now.
+    # Save a screenshot with our custom file name and time_stamp in a specific folder.
+    # For this, the folder is "screenshots" in the test directory.  This will fail if the folder
+    # is not present (but it's fair to assume that it will be, so I'm not bothering with a check).
     selenium.get_screenshot_as_file(util.os_safe_screenshot_path() + test_name + time_stamp + ".png")
-    # Verify that we wrote "I <3 iframes" in TinyMCE.
+    # Verify that we wrote "Bold text is bold" in TinyMCE.
     assert "Bold text is bold" in selenium.find_element_by_xpath(variables["tinymce_text_field"]).text
