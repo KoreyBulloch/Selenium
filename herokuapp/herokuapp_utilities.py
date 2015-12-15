6f818d7b2135d12d1dbf6698166760e0802fb57c
KoreyBulloch <a-koreyb@hotmail.com>
2015-12-15 10:38:59 -0800

    Screenshot folder detection and creation.

--- herokuapp/herokuapp_utilities.py
+++ herokuapp/herokuapp_utilities.py
@@ -2,8 +2,21 @@
 
 
 import pytest
-import time
+import os
 
 
-class Utils():
-    def login(selenium, variables):
+class Util():
+    
+    def __init__(self, selenium, variables, request):
+        self.selenium  = selenium
+        self.variables = variables
+        self.request = request
+
+
+    def os_safe_screenshot_path(self):
+        return os.path.join(self.variables["screenshot_path"])
+
+
+    def check_and_create_screenshot_folder(self):
+        if os.path.exists("screenshot") != True:
+            os.makedirs("screenshot")
