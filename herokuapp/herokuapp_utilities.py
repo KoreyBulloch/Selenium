9c50a92e385e45988450973f1df988a6c76d0085
KoreyBulloch <a-koreyb@hotmail.com>
2015-12-15 10:45:01 -0800

    Updated comments

--- herokuapp/herokuapp_utilities.py
+++ herokuapp/herokuapp_utilities.py
@@ -13,10 +13,13 @@ class Util():
         self.request = request
 
 
+    # Creates a screenshot path that is safe for all operating systems.
     def os_safe_screenshot_path(self):
         return os.path.join(self.variables["screenshot_path"])
 
 
+    # Checks for a screenshot folder in the current directory, and
+    # creates one if it is not found.
     def check_and_create_screenshot_folder(self):
         if os.path.exists("screenshot") != True:
             os.makedirs("screenshot")
